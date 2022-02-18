13f49e67f26d8b182b1200bae9d6b5849dd8dc6e
Milind Sharma <milindsharma8@gmail.com>
2022-02-18 23:34:24 +0530

    Add make_api_request and check_status_code methods

--- ozone/ozone.py
+++ ozone/ozone.py
@@ -1,7 +1,10 @@
 import pandas
 import requests
 import json
+
 from urls import URLs
+
+# TODO: Add try-except blocks
 
 class Ozone():
     _search_aqi_url: str = URLs.search_aqi_url
@@ -9,15 +12,31 @@ class Ozone():
 
     def __init__(self, token: str = ''):
         self.token: str = token
-
         self._check_token_validity()
     
+    # TODO: Fix this method
     def _check_token_validity(self) -> None:
-        city_for_check: str = 'london'
-        url: str = f'{self._search_aqi_url}/{city_for_check}/?token={self.token}'
+        test_city: str = 'london'
+        r = self._make_api_request(f'{self._search_aqi_url}/{test_city}/?token={self.token}')
+
+        if self._check_status_code(r):
+            if json.loads(r.content)['status'] != 'ok':
+                print('Warning: Token may be invalid!')
+
+    def _make_api_request(self, url: str) -> requests.Response:
         r = requests.get(url)
-        if json.loads(r.content)['status'] != 'ok':
-            print('Warning: Token may be invalid!')
+        return r
+
+    def _check_status_code(self, r: requests.Response) -> bool:
+        if r.status_code == 200:
+            return True
+        elif r.status_code == 401:
+            raise Exception('Unauthorized!')
+        elif r.status_code == 404:
+            raise Exception('Not Found!')
+        elif r.status_code == 500:
+            raise Exception('Internal Server Error!')
+        return False
 
     def reset_token(self, token: str) -> None:
         """Use this method to set your API token
@@ -28,8 +47,25 @@ class Ozone():
         self.token = token
         self._check_token_validity()
 
+    def get_city_air(
+            self,
+            city: str,
+            df: pandas.DataFrame = pandas.DataFrame(),
+        ):
+        """Get the air quality of a city
 
-    # Needs to have method to get aqi data for city in 
+        Args:
+            city (str): The name of the city
+            df (pandas.DataFrame, optional): Existing dataframe, if one exists. Defaults to new empty pandas.DataFrame().
+
+        Returns:
+            pandas.DataFrame: The dataframe containing the air quality of the city
+        """
+        r = self._make_api_request(f'{self._search_aqi_url}/{city}/?token={self.token}')
+        if self._check_status_code(r):
+            # Get all the data.
+            data_obj = json.loads(r.content)['data']
+
 
 
 if __name__ == '__main__':
