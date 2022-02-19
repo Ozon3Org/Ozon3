690aa865e9ff2906fab9df96b72670f9bcf5c954
Milind Sharma <milindsharma8@gmail.com>
2022-02-19 13:16:26 +0530

    Fix minor bugs

--- ozone/ozone.py
+++ ozone/ozone.py
@@ -12,8 +12,8 @@ class Ozone():
     _search_aqi_url: str = URLs.search_aqi_url
     _find_stations_url: str = URLs.find_stations_url
     _default_params: List[str] = [
-                'aqi', 'pm25', 'pm10', 'o3', 'co', 'no2','so2', 'dew', 'h',
-                'p', 't','w', 'wg']
+        'aqi', 'pm25', 'pm10', 'o3', 'co', 'no2','so2', 'dew', 'h', 'p', 't', 'w', 'wg'
+        ]
 
     def __init__(self, token: str = ''):
         self.token: str = token
@@ -24,7 +24,9 @@ class Ozone():
         """Check if the token is valid
         """
         test_city: str = 'london'
-        r = self._make_api_request(f'{self._search_aqi_url}/{test_city}/?token={self.token}')
+        r = self._make_api_request(
+            f'{self._search_aqi_url}/{test_city}/?token={self.token}'
+            )
 
         if self._check_status_code(r):
             if json.loads(r.content)['status'] != 'ok':
@@ -68,20 +70,29 @@ class Ozone():
             self,
             city: str,
             df: pandas.DataFrame = pandas.DataFrame(),
-            params: List[str] = None,
+            params: List[str] = [''],
         ):
-        if params == None:
+        if params == ['']:
             params = self._default_params
-        r = self._make_api_request(f'{self._search_aqi_url}/{city}/?token={self.token}')
+
+        r = self._make_api_request(
+            f'{self._search_aqi_url}/{city}/?token={self.token}'
+            )
         if self._check_status_code(r):
             # Get all the data.
             data_obj = json.loads(r.content)['data']
             row = self._parse_data(data_obj, city, params)
-            df = df.append(row, ignore_index=True) # TODO: Don't use append. deprecated warning.
+            df = df.append(row, ignore_index=True)
+            # TODO: Don't use append. deprecated warning.
         
         return df
 
-    def _parse_data(self, data_obj, city: str, params: List[str]) -> Dict[str, Union[str, float]]:
+    def _parse_data(
+        self,
+        data_obj: Any,
+        city: str,
+        params: List[str]
+        ) -> Dict[str, Union[str, float]]:
         """Parse the data from the API response
 
         Args:
@@ -90,7 +101,8 @@ class Ozone():
         Returns:
             dict: The parsed data.
         """
-        row: Dict[str, Union[str, float]] = {} # A single row of data for the dataframe.
+        # A single row of data for the dataframe.
+        row: Dict[str, Union[str, float]] = {} 
         
         try:
             row['city'] = f'{city}'
@@ -114,4 +126,5 @@ class Ozone():
         return row
 
 if __name__ == '__main__':
-    pass
+    test = Ozone(token = '0e0fc4695a0a368fc63ae484dcbfb58953a20985')
+    print(test.get_city_air('london'))
