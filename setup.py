1b8cad75322daaa177aa5461b336d098f3f84654
Milind Sharma <milindsharma8@gmail.com>
2022-02-21 00:30:04 +0530

    Further changes to setup.py

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ setuptools.setup(
     version="1.0.2",
     packages=setuptools.find_packages(),
     install_requires=[
-        "numpy",
-        "pandas",
-        "requests",
+        "numpy; python_version>='3'",
+        "pandas; python_version>='3'",
+        "requests; python_version>='3'",
     ],
     python_requires=">=3.6",
     classifiers=[
