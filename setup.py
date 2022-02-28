c3da1ac59bfc7c1d457b491e66bf97a1230bc015
Milind Sharma <milindsharma8@gmail.com>
2022-02-28 19:30:14 +0530

    Update classifiers

--- setup.py
+++ setup.py
@@ -24,7 +24,9 @@ setuptools.setup(
         "Intended Audience :: Developers",  # Define that your audience are developers
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
