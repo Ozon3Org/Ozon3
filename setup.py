ca58dea710e47248338762a47373c6b547bf1a19
Milind Sharma <milindsharma8@gmail.com>
2022-03-22 12:14:36 +0530

    chore: Bump release version to 1.4.6

--- setup.py
+++ setup.py
@@ -9,19 +9,19 @@ setuptools.setup(
     description="A package to get air quality data using the WAQI API",
     license="GPLv3+",
     url="https://github.com/Milind220/Ozone",
-    version="1.4.5",
-    download_url="https://github.com/Milind220/Ozone/archive/refs/tags/v1.4.5.tar.gz",
+    version="1.4.6",
+    download_url="https://github.com/Milind220/Ozone/archive/refs/tags/v1.4.6.tar.gz",
     packages=setuptools.find_packages(),
     install_requires=[
         "numpy; python_version>='3'",
         "pandas; python_version>='3'",
         "requests; python_version>='3'",
         "openpyxl; python_version>='3'",
-        "ratelimit; python_version>='3'"
+        "ratelimit; python_version>='3'",
     ],
     python_requires=">=3.6",
     classifiers=[
-        "Development Status :: 5 - Production/Stable",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",  # Define that your audience are developers
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
