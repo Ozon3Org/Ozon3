6d1a5655d68122579a38494f8e36f6fe283ab099
Nuclear03020704 <52926983+lahdjirayhan@users.noreply.github.com>
2022-04-03 23:12:50 +0700

    style: Format using black

--- src/ozone/historical/_reverse_engineered.py
+++ src/ozone/historical/_reverse_engineered.py
@@ -10,11 +10,12 @@ from sseclient import SSEClient as SSE
 # The following variable is a long string, a source JS code that
 # is excerpted from one of aqicn.org frontend's scripts.
 # See relevant_funcs.js for more information.
-_JS_FUNCS = Path(__file__).parent.joinpath('relevant_funcs.js').read_text()
+_JS_FUNCS = Path(__file__).parent.joinpath("relevant_funcs.js").read_text()
 
 # Make js context where js code can be executed
 _context = js2py.EvalJs()
 _context.execute(_JS_FUNCS)
+
 
 def get_results_from_backend(city_id: int) -> List[Dict[str, Any]]:
     event_data_url = f"https://api.waqi.info/api/attsse/{city_id}/yd.json"
@@ -23,40 +24,43 @@ def get_results_from_backend(city_id: int) -> List[Dict[str, Any]]:
     result = []
 
     for event in events:
-        if event.event == 'done':
+        if event.event == "done":
             break
 
         try:
-            if 'msg' in event.data:
+            if "msg" in event.data:
                 result.append(json.loads(event.data))
         except json.JSONDecodeError:
             pass
 
     return result
 
+
 def parse_incoming_result(json_object: dict) -> pandas.DataFrame:
     # Run JS code
     # Function is defined within JS code above
     # Convert result to Python dict afterwards
     OUTPUT = _context.gatekeep_convert_date_object_to_unix_seconds(
-        json_object['msg']
+        json_object["msg"]
     ).to_dict()
 
     # Change unix timestamp back to datetime
     RESULT = OUTPUT
-    for i, spec in enumerate(OUTPUT['species']):
-        for j, val in enumerate(spec['values']):
-            RESULT['species'][i]['values'][j]['t']['datetime'] = datetime.fromtimestamp(OUTPUT['species'][i]['values'][j]['t']['d'])
+    for i, spec in enumerate(OUTPUT["species"]):
+        for j, val in enumerate(spec["values"]):
+            RESULT["species"][i]["values"][j]["t"]["datetime"] = datetime.fromtimestamp(
+                OUTPUT["species"][i]["values"][j]["t"]["d"]
+            )
 
     result_dict = {}
-    for spec in OUTPUT['species']:
-        pollutant_name: str = spec['pol']
+    for spec in OUTPUT["species"]:
+        pollutant_name: str = spec["pol"]
 
         dates, values = [], []
-        for step in spec['values']:
+        for step in spec["values"]:
             # Change unix timestamp back to datetime
-            date = datetime.fromtimestamp(step['t']['d'])
-            value: int = step['v']
+            date = datetime.fromtimestamp(step["t"]["d"])
+            value: int = step["v"]
 
             dates.append(date)
             values.append(value)
@@ -67,14 +71,13 @@ def parse_incoming_result(json_object: dict) -> pandas.DataFrame:
     FRAME = pandas.DataFrame(result_dict)
     return FRAME
 
+
 def get_data_from_id(city_id: int) -> pandas.DataFrame:
     backend_data = get_results_from_backend(city_id)
-    result = pandas.concat([
-        parse_incoming_result(data) for data in backend_data
-    ])
+    result = pandas.concat([parse_incoming_result(data) for data in backend_data])
 
     # Arrange to make most recent appear on top of DataFrame
-    result = result.sort_index(ascending = False, na_position = 'last')
+    result = result.sort_index(ascending=False, na_position="last")
 
     # Deduplicate because sometimes the backend sends duplicates
     result = result[~result.index.duplicated()]
@@ -85,7 +88,7 @@ def get_data_from_id(city_id: int) -> pandas.DataFrame:
     #  response AQI data was empty.
     if len(result) > 1:
         complete_days = pandas.date_range(
-            result.index.min(), result.index.max(), freq='D'
+            result.index.min(), result.index.max(), freq="D"
         )
         result = result.reindex(complete_days, fill_value=None)
 
